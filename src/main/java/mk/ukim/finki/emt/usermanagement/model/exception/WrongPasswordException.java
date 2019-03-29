21f947357f0321273b6aeeac84c283abc48fb20a
unknown <stefan.andonov@students.finki.ukim.mk>
2019-03-29 15:42:48 +0100

    school done

---
+++ src/main/java/mk/ukim/finki/emt/usermanagement/model/exception/WrongPasswordException.java
@@ -0,0 +1,4 @@
+package mk.ukim.finki.emt.usermanagement.model.exception;
+
+public class WrongPasswordException extends Exception {
+}
--- src/main/java/mk/ukim/finki/emt/usermanagement/service/UsersService.java
+++ src/main/java/mk/ukim/finki/emt/usermanagement/service/UsersService.java
@@ -1,19 +1,30 @@
 package mk.ukim.finki.emt.usermanagement.service;
 
 import mk.ukim.finki.emt.usermanagement.model.User;
-import mk.ukim.finki.emt.usermanagement.model.exception.UserDoesNotExistsException;
-import mk.ukim.finki.emt.usermanagement.model.exception.UsernameAlreadyExistsException;
-import mk.ukim.finki.emt.usermanagement.model.exception.WeakExceptionPassword;
-import mk.ukim.finki.emt.usermanagement.model.exception.WrongActivationCodeException;
+import mk.ukim.finki.emt.usermanagement.model.exception.*;
+
+import java.util.List;
 
 public interface UsersService {
 
 
+    User createUnconfirmedUser(String username, String name, String password, String email)
+            throws UsernameAlreadyExistsException, WeakExceptionPassword;
 
-    User createUnconfirmedUser (String username, String name, String password, String email)
-    throws UsernameAlreadyExistsException, WeakExceptionPassword;
-
-    User confirmUser (String username, String password, String activationCode)
+    User confirmUser(String username, String password, String activationCode)
             throws UserDoesNotExistsException, WrongActivationCodeException;
+
+    User login(String username, String password) throws UserDoesNotExistsException, WrongPasswordException;
+
+    boolean logout(String username);
+
+    User changePassword(String username, String oldPassword, String newPassword) throws WeakExceptionPassword;
+
+    List<User> getAllUsers(Long requestUserId);
+
+    User upadateUserUsername (String username, String newUsername) throws
+            UsernameAlreadyExistsException;
+
+
 
 }
